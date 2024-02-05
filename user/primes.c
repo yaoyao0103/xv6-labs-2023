875e8fd7dfb4a92258fe47d3f07886c370becdb4
yaoyao0103 <elites96105@gmail.com>
2024-02-06 00:46:27 +0800

    fisish primes

--- user/primes.c
+++ user/primes.c
@@ -2,63 +2,61 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define MAXNUM 36
+#define MAX 36
+
 
 void
-helper(int read_fd, int write_fd)
+helper(int fd[])
 {
-	char nums[MAXNUM];
-	read(read_fd, nums, sizeof(nums));
-	int temp = 0;
-	for(int i = 0; i < MAXNUM; i++)
-		if(nums[i] == '0'){
+	char nums[MAX];
+	read(fd[0], nums, sizeof(nums));
+	int first = 0;
+	for(int i = 0; i < MAX; i++){
+		if(nums[i] == '0' && first == 0){
 			printf("prime %d\n", i);
+			first = i;
 			nums[i] = '1';
-			temp = i;	
-			break;
 		}
-	if(temp == 0) exit(0);
-	for(int i = 0; i < MAXNUM; i++) if(i % temp == 0) nums[i] = '1';
-	int pid = fork();
-	if(pid == -1){
-		fprintf(1, "fork error\n");
-		exit(1);
+		else if(first != 0 && i % first == 0) nums[i] = '1';
 	}
-	// parent
+	if(first == 0) exit(0);
+	
+	int pid;	
+	if((pid = fork()) == 0){
+		helper(fd);
+		wait(0);
+	}
 	else if(pid > 0){
-		write(write_fd, nums, sizeof(nums));
-		wait(0);
+		write(fd[1], nums, sizeof(nums));
+		wait(0);		
 	}
-	else if(pid == 0){
-		helper(read_fd, write_fd);
-		wait(0);
+	else{
+		fprintf(1, "Error: fork error\n");
+		exit(1);
 	}
 }
 
 int
 main(int argc, char* argv[])
 {
-	int fd[2];
-	char nums[MAXNUM];
-
+	char nums[MAX];
 	memset(nums, '0', sizeof(nums));
+	int fd[2];
 	pipe(fd);
-	int pid = fork();
-	if(pid == -1){
-		fprintf(1, "fork error\n");
-		exit(1);
+	int pid;
+	if((pid = fork()) == 0){
+		helper(fd);
+		wait(0);
 	}
-	// parent
 	else if(pid > 0){
 		nums[0] = '1';
 		nums[1] = '1';
 		write(fd[1], nums, sizeof(nums));
 		wait(0);
 	}
-	// child
-	else if(pid == 0){
-		helper(fd[0], fd[1]);
-		wait(0);
+	else{
+		fprintf(1, "Error: fork error\n");
+		exit(1);
 	}
 	exit(0);
 }
