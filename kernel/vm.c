64010519ccb70092ed42557572bf71214ddf2569
yaoyao0103 <elites96105@gmail.com>
2024-02-09 16:43:56 +0800

    finish print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,31 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void
+vmprinter(pagetable_t pagetable, int layer)
+{
+	for(int i = 0; i < 512; i++){
+		pte_t pte = pagetable[i];
+		// this points to a lower-level page table.
+		if(pte & PTE_V){
+			for(int j = 0; j < layer; j++) printf(" ..");
+			printf("%d: pte: %p pa %p\n", i, (uint64)pte, PTE2PA(pte));
+		}
+		if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+			uint64 child = PTE2PA(pte);
+			vmprinter((pagetable_t)child, layer+1);
+			//pagetable[i] = 0;
+		}
+	}
+
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+	printf("page table %p\n", pagetable);
+	vmprinter(pagetable, 1);
+}
+
