0513e0d568baaafbeabfe523c048ac75865a4560
yaoyao0103 <elites96105@gmail.com>
2024-02-11 00:49:00 +0800

    backup

--- kernel/vm.c
+++ kernel/vm.c
@@ -456,15 +456,15 @@ vmprinter(pagetable_t pagetable, int layer)
 {
 	for(int i = 0; i < 512; i++){
 		pte_t pte = pagetable[i];
-		// this points to a lower-level page table.
+		// only print valid entry
 		if(pte & PTE_V){
 			for(int j = 0; j < layer; j++) printf(" ..");
 			printf("%d: pte: %p pa %p\n", i, (uint64)pte, PTE2PA(pte));
-		}
-		if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
-			uint64 child = PTE2PA(pte);
-			vmprinter((pagetable_t)child, layer+1);
-			//pagetable[i] = 0;
+			// this points to a lower-level page table.
+			if((pte & (PTE_R | PTE_W | PTE_X)) == 0){
+				uint64 child = PTE2PA(pte);
+				vmprinter((pagetable_t)child, layer+1);
+			}
 		}
 	}
 
